cb5dd7420eb43060e4b844bdd14cc7cb57fa46c5
201912025 <michi2012@naver.com>
2025-04-08 17:14:24 +0900

    feat: 초기설정 구현

---
+++ src/test/java/surveypulse/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package surveypulse.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
